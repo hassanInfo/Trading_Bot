a8a713238a40f7fcfd2e881b11062ffa7f7700d5
ABDERRAHIM JANBOUBI <81822929+JANBOUBI-ABDERRAHIM@users.noreply.github.com>
2023-01-16 19:50:12 +0100

    Update App.py

--- App.py
+++ App.py
@@ -147,7 +147,7 @@ def apply(model_name, data, title):
     # show_eval_result(model_name, test_result, initial_offset)
     st.write(visualize(data, history, title=title+str(format_position(test_result))))
 
-st.title('                          Welcome to the Trading world\nwe are here to provide you the best Market order                           ')
+st.markdown('<h1 align="center">Welcome to the Trading world we are here to provide you the best Market order</h1>')
 st.markdown('<b>Please select a currency or a stock:', unsafe_allow_html=True)
 option = st.selectbox(
     '',
