e6833578398a6ca762651c91856adc0136f7b899
ABDERRAHIM JANBOUBI <81822929+JANBOUBI-ABDERRAHIM@users.noreply.github.com>
2023-01-16 20:05:54 +0100

    Update App.py

--- App.py
+++ App.py
@@ -147,13 +147,14 @@ def apply(model_name, data, title):
     # show_eval_result(model_name, test_result, initial_offset)
     st.write(visualize(data, history, title=title+str(format_position(test_result))))
 
-st.markdown('<h1 align="center">Welcome to the Trading world</h1>', unsafe_allow_html=True)
+st.markdown('<h1 align="center">Welcome to the Trading world!!!</h1>', unsafe_allow_html=True)
 st.markdown('<h1 align="center">We are here to provide you the best Market order </h1>', unsafe_allow_html=True)
 st.markdown('<h1 align="center"></h1>', unsafe_allow_html=True)
 st.markdown('<h1 align="center"></h1>', unsafe_allow_html=True)
 st.markdown('<h1 align="center"></h1>', unsafe_allow_html=True)
+st.markdown('<h4 align="center">Please select a currency or a stock:</h4>', unsafe_allow_html=True)
 option = st.selectbox(
-    'Please select a currency or a stock:',
+    '',
     ('Bitcoin', 'Etherium', 'Google', 'Apple')
 )
 
