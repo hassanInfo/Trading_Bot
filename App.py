8d6bad8fccdf0c74f8c5961a1e77dd74274daa72
ABDERRAHIM JANBOUBI <81822929+JANBOUBI-ABDERRAHIM@users.noreply.github.com>
2023-01-16 19:14:48 +0100

    Update App.py

--- App.py
+++ App.py
@@ -136,7 +136,7 @@ def visualize(df, history, title="trading session"):
 col1, col2, col3 = st.beta_columns([1,6,1])
 
 with col1:
-st.write("")
+    st.write("")
 
 def apply(model_name, data, title):
     window_size = 10
@@ -149,10 +149,10 @@ def apply(model_name, data, title):
     test_result, history = evaluate_model(agent, test_data, window_size, debug)
     # show_eval_result(model_name, test_result, initial_offset)
     with col2:
-    st.write(visualize(data, history, title=title+str(format_position(test_result))))
+        st.write(visualize(data, history, title=title+str(format_position(test_result))))
 
 with col3:
-st.write("")
+    st.write("")
 
 st.title('Welcome to the Trading world we are here to provide you the best Market order')
 st.markdown('<b>Please select a currency or a stock:', unsafe_allow_html=True)
